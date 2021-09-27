eaec39d003668f24068cea2ac0585e64b0b683e4
nealycaffrey <rk408@snu.edu.in>
2021-09-27 19:08:36 +0530

    added comments

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const fs = require('fs');
+const prompt = require("prompt-sync")();
 //import list from '/files.js';
 
-let corpus = []
+//initialized the corpus
+let corpus = [];
 
+//function to traverse the documents and add to the corpus
 function makeCorpus(docs) {
     for(let i in docs) {
       try {
         let data = fs.readFileSync(docs[i], {encoding: 'utf8'});
+        //does basic preprocessing to split into tokens and adds to corpus
         corpus.push(data.replace(/[\r\n]/g," ").trim().split(" "));
       } catch (err) {
         throw err;
@@ -15,7 +19,8 @@ function makeCorpus(docs) {
     return corpus;
   }
 
-makeCorpus(["docs/1.txt", "docs/2.txt", "docs/3.txt", "docs/4.txt", "docs/5.txt",
+makeCorpus(["docs/1.txt", "docs/2.txt", "docs/3.txt", 
+"docs/4.txt", "docs/5.txt",
  "docs/6.txt", "docs/7.txt", "docs/8.txt", "docs/9.txt", "docs/10.txt", "docs/11.txt", "docs/12.txt",
   "docs/13.txt", "docs/14.txt", "docs/15.txt", "docs/16.txt", "docs/17.txt", "docs/18.txt", "docs/19.txt", 
   "docs/20.txt", "docs/21.txt", "docs/22.txt", "docs/23.txt",
@@ -27,22 +32,32 @@ makeCorpus(["docs/1.txt", "docs/2.txt", "docs/3.txt", "docs/4.txt", "docs/5.txt"
   "docs/59.txt", "docs/60.txt", "docs/61.txt", 
 ]);
 
-
+//length of the document, in order to perform document normalization
 let N = corpus.length;
 
+/**
+ * calculates the term frequency of a particular term in a document
+ * term frequency is computed as:
+ * number of occurrences of term/ document length
+ */
 function termFreq(term, doc) {
     let frequency = 0;
-    var result;
     let lower = term.toLowerCase();
+    let doc_length = doc.length +1;
     for (let i in doc){
       if (doc[i].toLowerCase() == lower){
         frequency++;
       }
     }
-    result = (frequency * 1.0 / (doc.length + 1));
-    return result;
+    //return normalized term frquency
+    return frequency/ doc_length;
   }
 
+
+  /**
+   * calculates inverse document frequency of a term in a particular document
+   * idf = log(no. of docs the term appears in/ term freq)
+   */
   function invDocFreq(term) {
     if (corpus == null){
         return -1;
@@ -62,6 +77,10 @@ function termFreq(term, doc) {
     }
   }
 
+  /**
+   * creates an idf model, an individual vector of the idf
+   * of the given input query term in a document
+   */
   function createIdf(query) {
     let model = [];
     query instanceof Array ? query: query.split(" ");
@@ -72,9 +91,11 @@ function termFreq(term, doc) {
     return model;
   }
 
-//   let vector = createIdf('content of doc');
-//   console.log(vector);
 
+/**
+ * creates a vector for tf-idf values for every given query
+ * multiplies tf by idf and returns vector
+ */
   function createVSM(query, doc) {
     let tf = [];
     let vsm = []
@@ -96,7 +117,11 @@ function termFreq(term, doc) {
     return vsm;
   }
 
-  //between two vectors
+  /**
+   * finds the cosine similarity between a pair of vectors
+   * cosine similarity is directly proportional to the relevance 
+   * of the query to the document
+   */
   function cosineSimilarity(query, doc){
     let sim_idx = 0;
 
@@ -104,24 +129,21 @@ function termFreq(term, doc) {
     let q_vec = createVSM(query, query);
     let doc_vec = createVSM(query, doc);
 
-    let query_mag = findMag(q_vec);
-    let doc_mag = findMag(doc_vec);
-
     for (let i in query){
       isNaN(q_vec[i] * doc_vec[i])? sim_idx += 0: sim_idx += q_vec[i] * doc_vec[i];
     }
-    let mag_product = query_mag * doc_mag;
+    let product = findMag(q_vec) * findMag(doc_vec);
 
-    if(isNaN(1.0 * sim_idx / (mag_product))){
+    if(isNaN(1.0 * sim_idx / (product))){
         return 0;
     } else {
-        return 1.0 * sim_idx / (mag_product);
+        return 1.0 * sim_idx / (product);
     }
   }
 
 
   /*
-  * Ranks the documents in your corpus according to a query
+  * ranks the documents in the given corpus according to a query
   */
   function rank(query){
     let ranking = [];
@@ -134,23 +156,24 @@ function termFreq(term, doc) {
         index: i,
         });
     }
+    //sorts the documents based on the cosine similarity values of the document
     ranking.sort((a, b) => {
       return b.similarityIndex - a.similarityIndex;
     })
 
     let ranks = [];
-
+    
+    //finds the top 10 relevant documents to the given query
     ranking.slice(0,10).map((val)=>{
       //console.log(val.index);
       ranks.push(val.similarityIndex, val.index + 1);
     });
-
     return ranks;
   }
 
 
 /*
-* Calculates the magnitude of an input vector
+* function to find the magnitude of a given input vector
 */
   function findMag(vec) {
     let mag = 0
@@ -158,13 +181,14 @@ function termFreq(term, doc) {
       if (isNaN(vec[i])) {
         mag += 0;
       } else {
-        mag += vec[i] * vec[i];
+        mag += (vec[i] * vec[i]);
       }
     }
     //console.log(Math.sqrt(mag));
     return Math.sqrt(mag);
   }
 
-  let search_result = rank("bingley is a bad man");
-  console.log(search_result);
+
+  const input_query = prompt('Enter your query: ');
+  console.log(rank(input_query));
 
